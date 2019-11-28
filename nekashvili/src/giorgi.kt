a4607a0df3611477278c2be32ecb675032a89399
gioneka <58290193+gioneka@users.noreply.github.com>
2019-11-28 12:58:13 +0400

    initial

---
+++ nekashvili/src/giorgi.kt
@@ -0,0 +1,18 @@
+fun main() {
+    val z = Transport.MARSHUTKA.moculoba
+    val y = Transport.MARSHUTKA.fasi
+    val x = Transport.MARSHUTKA.modzraoba
+    print("ამ ტრანსპორტში ეტევა $z ადამიანი, ფასია $y ლარი და მოძრაობს $x")
+}
+enum class Transport(val fasi: Float,val modzraoba:String,val moculoba: Int) {
+// კონსტრუქტორი
+    // აქ enum-ებს გადაეცემა პარამეტრი weather და holiday
+
+    AVTOBUSI(0.20F,"ნელა",52),
+    MARSHUTKA(0.8F,"საშუალო",25),
+    METRO(0.2F,"ძალიან სწრაფი",300),
+    SHMAXI(5F,"სწრაფი",4),
+    SABAGIRO(3F,"საშუალო",7)
+}
+
+
